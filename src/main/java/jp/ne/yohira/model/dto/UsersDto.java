fe11d17d9df6b519aac91e883e8f30ae2a01da8f
yohira0616 <yo.hira.dev@gmail.com>
2016-01-30 19:48:00 +0900

    ログイン画面の実装

--- src/main/java/jp/ne/yohira/model/dto/UsersDto.java
+++ src/main/java/jp/ne/yohira/model/dto/UsersDto.java
@@ -1,10 +1,13 @@
 package jp.ne.yohira.model.dto;
 
+import org.springframework.data.annotation.Id;
+
 import lombok.Data;
 
 @Data
 public class UsersDto {
 
+	@Id
 	private String userId;
 	private String passwd;
 
