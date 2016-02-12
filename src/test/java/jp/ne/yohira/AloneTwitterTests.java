2208e295eb99a981eccd194d305f4d9b2f15c407
yohira0616 <yo.hira.dev@gmail.com>
2016-02-13 01:05:08 +0900

    TODO テストコード

---
+++ src/test/java/jp/ne/yohira/AloneTwitterTests.java
@@ -0,0 +1,19 @@
+package jp.ne.yohira;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AloneTwitterApplication.class)
+@WebAppConfiguration
+public class AloneTwitterTests {
+
+	@Test
+	public void test() {
+		System.out.println("TODO implement Test");
+	}
+
+}
