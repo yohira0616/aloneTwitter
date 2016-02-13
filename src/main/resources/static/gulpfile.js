d501be7639514ac9690cf4cfda40159d70709d1c
yohira0616 <yo.hira.dev@gmail.com>
2016-02-14 01:49:16 +0900

    gulpfile更新

--- src/main/resources/static/gulpfile.js
+++ src/main/resources/static/gulpfile.js
@@ -3,21 +3,29 @@ var scss = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var webpack = require('gulp-webpack');
 var babel = require('gulp-babel');
+var plumber = require('gulp-plumber');
+
+//PATH
 var TARGET_PATH = '../../../../target/classes/static/';
 var THYMELEAF_PATH = '../../../../target/classes/templates/';
 
-gulp.task('scss', function () {
+//command definition
+var scssCompile = 'stylesheet';
+var jsCompile = 'javascript';
+var htmlCopy = 'thymeleaf';
+var imgCopy = 'img';
+
+gulp.task(scssCompile, function () {
     gulp.src('scss/**/*.scss')
+        .pipe(plumber())
         .pipe(scss())
-        .pipe(gulp.dest(TARGET_PATH + 'css'));
+        .pipe(gulp.dest(TARGET_PATH + 'css'))
+        .pipe(gulp.dest('css'));
 });
 
-gulp.task('scss:watch', function () {
-    gulp.watch('scss/**/*.scss', ['scss']);
-});
-
-gulp.task('jscompile', function () {
+gulp.task(jsCompile, function () {
     gulp.src('js/**/twitter.js')
+        .pipe(plumber())
         .pipe(webpack({
             output: {
                 filename: 'twitter.min.js'
@@ -25,26 +33,25 @@ gulp.task('jscompile', function () {
         }))
         .pipe(babel())
         .pipe(uglify())
-        .pipe(gulp.dest(TARGET_PATH + 'js'));
+        .pipe(gulp.dest(TARGET_PATH + 'js'))
+        .pipe(gulp.dest('js'));
 });
 
-gulp.task('thymeleaf', function () {
+gulp.task(htmlCopy, function () {
     gulp.src('../templates/**/*.html')
         .pipe(gulp.dest(THYMELEAF_PATH));
 });
 
-gulp.task('img', function () {
+gulp.task(imgCopy, function () {
     gulp.src('img/**/*')
         .pipe(gulp.dest(TARGET_PATH + "img"));
 });
 
-gulp.task('jswatch', function () {
-    gulp.watch('js/**/*.js', ['jscompile']);
-});
+gulp.task('default', [scssCompile, jsCompile, htmlCopy, imgCopy]);
 
-gulp.task('allwatch', function () {
-    gulp.watch('scss/**/*.scss', ['scss']);
-    gulp.watch('js/**/*.js', ['jscompile']);
-    gulp.watch('img/**/*', ['img']);
-    gulp.watch('../templates/**/*.html', ['thymeleaf']);
+gulp.task('watch', function () {
+    gulp.watch('scss/**/*.scss', [scssCompile]);
+    gulp.watch('js/**/*.js', [jsCompile]);
+    gulp.watch('img/**/*', [imgCopy]);
+    gulp.watch('../templates/**/*.html', [htmlCopy]);
 });
