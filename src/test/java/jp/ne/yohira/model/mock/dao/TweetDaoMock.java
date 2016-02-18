381b2869e03232d0ad0841f5aa102bb06b58938d
yohira0616 <yo.hira.dev@gmail.com>
2016-02-18 22:53:12 +0900

    Fluentlenium

---
+++ src/test/java/jp/ne/yohira/model/mock/dao/TweetDaoMock.java
@@ -0,0 +1,39 @@
+package jp.ne.yohira.model.mock.dao;
+
+import java.util.Calendar;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import jp.ne.yohira.model.dao.TweetDaoSpec;
+import jp.ne.yohira.model.dto.TweetDto;
+
+public class TweetDaoMock implements TweetDaoSpec {
+
+	@Override
+	public List<TweetDto> getTweets() {
+		List<TweetDto> mock = Lists.newArrayList();
+		TweetDto dto = new TweetDto();
+		dto.setPostId(0);
+		dto.setContents("mock");
+		dto.setPrcDate(Calendar.getInstance().getTime());
+		mock.add(dto);
+		return mock;
+	}
+
+	@Override
+	public void delete(long postId) {
+
+	}
+
+	@Override
+	public void createTweet(TweetDto dto) {
+
+	}
+
+	@Override
+	public long getNewPostId() {
+		return 1;
+	}
+
+}
---
+++ src/test/java/jp/ne/yohira/model/service/AloneTwitterServiceTest.java
@@ -0,0 +1,16 @@
+package jp.ne.yohira.model.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class AloneTwitterServiceTest {
+
+	@Test
+	@Ignore
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
