06643ed7b114d5776077ef36731edf9e0bcb1b7e
よーひら <yo.hira.dev@gmail.com>
2016-02-28 16:55:18 +0900

    add serial

--- src/main/java/jp/ne/yohira/model/auth/UsersDetails.java
+++ src/main/java/jp/ne/yohira/model/auth/UsersDetails.java
@@ -1,16 +1,19 @@
 package jp.ne.yohira.model.auth;
 
+import jp.ne.yohira.model.dto.UsersDto;
+
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 
-import jp.ne.yohira.model.dto.UsersDto;
-
 public class UsersDetails extends User {
+
+	private static final long serialVersionUID = 1L;
 
 	private UsersDto user;
 
 	public UsersDetails(UsersDto user) {
-		super(user.getUserId(), user.getPasswd(), AuthorityUtils.createAuthorityList("ROLE_USER"));
+		super(user.getUserId(), user.getPasswd(), AuthorityUtils
+				.createAuthorityList("ROLE_USER"));
 		this.user = user;
 	}
 
