a47992e5a07b6c934becc2c4e5e00326d3416632
yohira0616 <yukito.sys.so@gmail.com>
2016-03-24 00:25:18 +0900

    ユーザの作成をSQLに外出し

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
 -- USERS INITIALIZE
 DELETE FROM USERS;
-
---INSERT INTO users(user_id,passwd) VALUES('admin',crypt('admin',gen_solt('bf',10)));
+--CREATE TEST USERS
+INSERT INTO users(user_id,passwd) VALUES('admin',crypt('admin',gen_salt('bf',10)));
+INSERT INTO users(user_id,passwd) VALUES('testuser',crypt('testuser',gen_salt('bf',10)));
+INSERT INTO users(user_id,passwd) VALUES('admin1',crypt('user1',gen_salt('bf',10)));
